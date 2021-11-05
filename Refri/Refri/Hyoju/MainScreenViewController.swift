39bdeb993baebcbe2c170d34ed8212221aafa072
simyoju <dawnofspring@daum.net>
2021-11-05 19:42:24 +0900

    little finish 1st cell

--- Refri/Refri/Hyoju/MainScreenViewController.swift
+++ Refri/Refri/Hyoju/MainScreenViewController.swift
@@ -13,8 +13,8 @@ class MainScreenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let categoryNib = UINib(nibName: "CategoryCell", bundle: nil)
-        collectionView.register(categoryNib, forCellWithReuseIdentifier: "CategoryCell")
+        let inventoryCellNib = UINib(nibName: "InventoryCollectionViewCell", bundle: nil)
+        collectionView.register(inventoryCellNib, forCellWithReuseIdentifier: "InventoryCell")
         
     }
     
@@ -23,28 +23,33 @@ class MainScreenViewController: UIViewController {
 extension MainScreenViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 3
+        return 1
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if section == 0{
-            return 12
-        } else {
-            return 1
-        }
+        return 3
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        if indexPath.section == 0 {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCell", for: indexPath) as! CategoryCell
-            cell.categoryLabel.text = "test"
+        
+        switch indexPath.item {
+        case 1:
             return cell
-        } else {
+        case 2:
+            return cell
+        default:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InventoryCell", for: indexPath) as! InventoryCollectionViewCell
+            
+            
             return cell
         }
         
+        
+        
     }
+    
+    
 
 
 }
