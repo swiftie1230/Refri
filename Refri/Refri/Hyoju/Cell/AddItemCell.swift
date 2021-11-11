c2ffbc53ae91747ef71ac70072a8b849cfd4ab6b
simyoju <dawnofspring@daum.net>
2021-11-11 15:44:24 +0900

    modify addItemCell

--- Refri/Refri/Hyoju/Cell/AddItemCell.swift
+++ Refri/Refri/Hyoju/Cell/AddItemCell.swift
@@ -10,12 +10,9 @@ import UIKit
 class AddItemCell: UICollectionViewCell {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var typePickerView: UIPickerView!
-    @IBOutlet weak var refiButton: UIButton!
-    @IBOutlet weak var frozenButton: UIButton!
-    @IBOutlet weak var roomButton: UIButton!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var isExpirationSwitch: UISwitch!
-    @IBOutlet weak var memoTextField: UITextField!
+    @IBOutlet weak var memoTextField: UITextView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
